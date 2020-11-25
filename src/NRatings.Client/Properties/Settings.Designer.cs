f6c1e582512b7f84420185449734ebfb388b087a
Mathieu Souphy <msouphy@gmail.com>
2020-11-25 21:32:00 +0100

    Removal of clickonce stuff

--- src/NRatings.Client/Properties/Settings.Designer.cs
+++ src/NRatings.Client/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace NRatings.Client.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace NRatings.Client.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("https://localhost:5001/api")]
+        [global::System.Configuration.DefaultSettingValueAttribute("https://localhost:5001")]
         public string ApiBaseUri {
             get {
                 return ((string)(this["ApiBaseUri"]));
