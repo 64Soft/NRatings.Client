7a6f00c2beffc75e82f1b8db0da96f47dd88fce4
Mathieu Souphy <msouphy@gmail.com>
2020-11-25 18:06:24 +0100

    Build automation stuff

--- src/NRatings.Client/Properties/AssemblyInfo.cs
+++ src/NRatings.Client/Properties/AssemblyInfo.cs
@@ -1,27 +1,8 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("NRatings")]
-[assembly: AssemblyDescription("Manually or automatically adjust the ratings of NASCAR Racing 2003 car files")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NRatings")]
-[assembly: AssemblyCopyright("Copyright © 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("930642d2-3cb5-4972-a9fc-0547eeba9269")]
-
-[assembly: AssemblyVersion("4.2.5.0")]
-[assembly: AssemblyFileVersion("4.2.5.0")]
-[assembly: AssemblyInformationalVersion("4.2.5.0")]
