883106f8ad1a18c1ffddf5328e231f2618f0842a
Mathieu Souphy <msouphy@gmail.com>
2020-11-26 17:59:21 +0100

    Build config

--- src/NRatings.Client/Properties/AssemblyInfo.cs
+++ src/NRatings.Client/Properties/AssemblyInfo.cs
@@ -1,7 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("NRatings")]
+[assembly: AssemblyTitle("NRatings.Client")]
+[assembly: AssemblyCompany("64Soft")]
+[assembly: AssemblyProduct("NRatings for NR2003")]
+[assembly: AssemblyDescription("Manually or automatically adjust the ratings of NASCAR Racing 2003 car files")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("(development)")]
 
 [assembly: ComVisible(false)]
 
