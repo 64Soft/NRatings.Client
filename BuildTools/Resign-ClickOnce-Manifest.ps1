8942252fdea35594787d470952a43ff4f605040f
Mathieu <mathieu@64soft.eu>
2026-03-27 23:28:54 +0100

    Update mage.exe path retrieval in PowerShell script

--- BuildTools/Resign-ClickOnce-Manifest.ps1
+++ BuildTools/Resign-ClickOnce-Manifest.ps1
@@ -1,17 +1,28 @@
-$publishDir = (Get-ChildItem "$(Build.SourcesDirectory)\src\NRatings.Client\bin\$(BuildConfiguration)\app.publish\Application Files\NRatings.Client_*" -Directory).FullName
+$mage = Get-ChildItem "C:\Program Files (x86)\Microsoft SDKs\Windows" -Recurse -Filter "mage.exe" -ErrorAction SilentlyContinue | 
+    Sort-Object FullName -Descending | 
+    Select-Object -First 1
+
+if (-not $mage) {
+    Write-Error "mage.exe not found on this agent"
+    exit 1
+}
+
+Write-Host "Using mage.exe from: $($mage.FullName)"
+
+$publishDir = (Get-ChildItem "$env:BUILD_SOURCESDIRECTORY\src\NRatings.Client\bin\$env:BUILDCONFIGURATION\app.publish\Application Files\NRatings.Client_*" -Directory).FullName
 $manifestPath = "$publishDir\NRatings.Client.exe.manifest"
-$appManifest = "$(Build.SourcesDirectory)\src\NRatings.Client\bin\$(BuildConfiguration)\app.publish\NRatings.Client.application"
+$appManifest = "$env:BUILD_SOURCESDIRECTORY\src\NRatings.Client\bin\$env:BUILDCONFIGURATION\app.publish\NRatings.Client.application"
 
 # Temporarily remove .deploy extensions
 Get-ChildItem "$publishDir" -Recurse -Filter "*.deploy" | Rename-Item -NewName { $_.Name -replace '\.deploy$', '' }
 
 # Re-sign manifest
 Push-Location $publishDir
-& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8.1 Tools\mage.exe" -Update $manifestPath
+& $mage.FullName -Update $manifestPath
 Pop-Location
 
 # Restore .deploy extensions
 Get-ChildItem "$publishDir" -Recurse -File | Where-Object { $_.Name -ne "NRatings.Client.exe.manifest" } | Rename-Item -NewName { $_.Name + ".deploy" }
 
 # Update deployment manifest
-& "C:\Program Files (x86)\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.8.1 Tools\mage.exe" -Update $appManifest -AppManifest $manifestPath
+& $mage.FullName -Update $appManifest -AppManifest $manifestPath
